31aa1783300d5a7b8a07030c6749cf4ba3b4b79a
ak-mishra03 <143195815+ak-mishra03@users.noreply.github.com>
2026-02-09 11:32:44 +0530

    added the functionality to calculate flood percentage for a time window

--- backend/app/services/ndwi.py
+++ backend/app/services/ndwi.py
@@ -1,4 +1,4 @@
-# /backend/app/services/ndwi.py
+# SmartInsurance/backend/app/services/ndwi.py
 
 from warnings import warn
 import ee 
@@ -20,6 +20,82 @@ def init_ee():
         ee.Authenticate()
         ee.Initialize(project = EE)
 
+def get_ndwi_image(aoi: float, start_date: str, end_date: str):
+   collection = (
+           ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+           .filterBounds(aoi)
+           .filterDate(start_date,end_date)
+           .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",60))
+           )
+
+   count = collection.size().getInfo()
+   if count == 0:
+       raise ValueError("No sentinel-2 images found for ths date range")
+
+   image = collection.median()
+   green = image.select("B3")
+   nir = image.select("B8")
+
+   ndwi = (
+           green.subtract(nir)
+           .divide(green.add(nir))
+           .rename("ndwi")
+           )
+
+   return ndwi
+
+
+def compute_flood_stats(
+        lat: float,
+        lon: float,
+        pre_start: str,
+        pre_end: str,
+        post_start: str,
+        post_end: str
+        ):
+    init_ee()
+
+    #area of intrest = 5KM around the given coordinate
+    aoi = ee.Geometry.Point([lon,lat]).buffer(5000)     
+
+    pre_ndwi = get_ndwi_image(aoi,pre_start,pre_end)
+    post_ndwi = get_ndwi_image(aoi,post_start,post_end)
+    
+    water_threshold = 0.0
+
+    pre_water = pre_ndwi.gt(water_threshold)
+    post_water = post_ndwi.gt(water_threshold)
+
+    flood_mask = post_water.And(pre_water.Not())
+
+    pixel_area = ee.Image.pixelArea()
+
+    flooded_area = flood_mask.multiply(pixel_area).reduceRegion(
+            reducer = ee.Reducer.sum(),
+            geometry = aoi,
+            scale = 10,
+            maxPixels = 1e9
+            )
+
+    total_area = pixel_area.reduceRegion(
+            reducer = ee.Reducer.sum(),
+            geometry = aoi,
+            scale = 10,
+            maxPixels = 1e9
+            )
+
+    flooded_m2 = flooded_area.get("ndwi").getInfo()
+    total_m2 = total_area.get("area").getInfo()
+
+    if flooded_m2 is None or total_m2 is None:
+        raise ValueError("Area calculation failed")
+
+    flood_percent = (flooded_m2/total_m2)
+
+    return {
+            "flooded_area_m2": flooded_m2,
+            "flooded_area_percent": flood_percent
+            }
 
 #computes the ndwi values for a given location in a given time range
 def compute_ndwi_stats(lat:float, lon:float, start_date:str,end_date:str):
