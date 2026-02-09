9b02633417bf1eec0d16b2dee9e7f5eea641940a
solxer <143195815+ak-mishra03@users.noreply.github.com>
2026-02-09 11:52:07 +0530

    added the functionality to calculate flood percentage for a time window (#1)

--- backend/app/api/flood.py
+++ backend/app/api/flood.py
@@ -1,7 +1,11 @@
+#SmartInsurance/backend/app/api/flood.py
+
 from fastapi import APIRouter
-from app.services.ndwi import compute_ndwi_stats
+from app.services.ndwi import compute_ndwi_stats, compute_flood_stats
 
 router = APIRouter(prefix="/flood", tags=["Flood Detection"])
+
+
 
 @router.post("/ndwi")
 def compute_ndwi(
@@ -17,6 +21,26 @@ def compute_ndwi(
             "date_range": [start_date,end_date],
             "ndwi_stats":stats
             }
+@router.post("/flood-damage")
+def flood_damage(
+        lat: float,
+        lon: float,
+        pre_start: str,
+        pre_end: str,
+        post_start: str,
+        post_end: str,
+        ):
+    stats = compute_flood_stats(
+            lat, lon, pre_start,pre_end,post_start,post_end
+            )
+
+    return {
+            "location": [lat,lon],
+            "pre_flood_range": [pre_start,pre_end],
+            "post_flood_range": [post_start,post_end],
+            "flood_stats": stats
+            }
+
 
 
 
